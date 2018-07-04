479dd8bf85945bad596a64b8400151dd877c2280
Zan Markan <zan.markan@gmail.com>
2018-07-04 06:22:47 +0100

    we're in async hell mkay

--- script/single_bar_parser.ts
+++ script/single_bar_parser.ts
@@ -1,7 +1,9 @@
 import * as request from 'request-promise';
 import * as cheerio from 'cheerio';
 import * as fs from 'fs';
-import { BarBase } from './bars_parser';
+import * as Bluebird from 'bluebird';
+
+// import { BarBase } from './bars_parser';
 
 const BASE_URL = "https://www.brewdog.com";
 const BARS_JSON = "../static/bars.json";
@@ -32,75 +34,94 @@ const SAMPLE_HTML = `<div class="barDetails ">
 </div>
 </div>`
 
+type OpeningHours = {
+    day: string,
+    times: string
+};
+
+type Social = {
+    network: string,
+    handle: string
+    url: string
+};
+
+type ContactDetails = {
+    address: string[],
+    email: string,
+    telephone: string,
+    location: {
+        lat: string,
+        lng: string
+    }
+}
+
 export type Bar = {
     id: string;
     title: string;
     url: string,
 
-    opening_hours: [
-        {
-            day: string,
-            timeOpen: string,
-            timeClose: string
-        }
-    ],
-
-    social: [
-        {
-            network: string,
-            handle: string
-            url: string
-        }
-    ],
-
-    contact_details: {
-        address: string[],
-        email: string,
-        telephone: string,
-        location: {
-            lat: string,
-            lng: string
-        }
-    }
+    opening_hours: OpeningHours[],
+    social: Social[],
+    contact_details: ContactDetails
 }
 
-const fullBars = Array<Bar>();
 
-let baseBars = fs.readFileSync("../static/bars.json").toJSON().data as Array<BarBase>;
-baseBars.forEach( bar => fullBars.push(requestBarDetails(bar)));
-fs.writeFileSync("../static/bars-full.json", JSON.stringify(fullBars));
 
-let requestBarDetails = (barBase: BarBase): Bar => {
+// let baseBars = baseBarsBuffer.toJSON().data as Array<BarBase>;
+
+export type BarBase = {
+    id: string;
+    title: string;
+    url: string
+}
+
+let requestBarDetails = (barBase: BarBase) => {
     let fullBar: Bar = {} as Bar;
 
-    request({
+    const options = {
         uri: barBase.url,
-       transform: function (body: any) {
-           return cheerio.load(body);
-       }
-   }).then( ($: CheerioAPI) => {
+        transform: function (body: any) {
+          return cheerio.load(body);
+        }
+    };
+
+    return request(options).then( ($: CheerioAPI) => {
 
         let barDeetsHtml = $("div.barDetails");
 
         let mapHtml = $(barDeetsHtml).find("div#map");
-        fullBar.contact_details.location.lat = mapHtml.attr("data-lat");
-        fullBar.contact_details.location.lat = mapHtml.attr("data-lng");
+        let location = {
+            lat: mapHtml.attr("data-lat"),
+            lng: mapHtml.attr("data-lng")
+        };
 
-        let addr = $(barDeetsHtml).find("script[src=https://maps.google.com/maps/api/js?sensor=false&libraries=places]").next().text();
-        fullBar.contact_details.address = addr.split(",");
+
+        let addr = $(barDeetsHtml).find("script[src*=maps\\.google\\.com]").next().text().split(",");
+        // fullBar.contact_details.address = addr.split(",");
 
         let tel = $(barDeetsHtml).find("p.tel").text();
-        fullBar.contact_details.telephone = tel;
+        // fullBar.contact_details.telephone = tel;
 
         //Email - needs to be decoded because fuck yeah
         let encodedEmail = $(barDeetsHtml).find("span.__cf_email__").attr("data-cfemail");
-        fullBar.contact_details.email = cfDecodeEmail(encodedEmail);
+        let email = cfDecodeEmail(encodedEmail);
+
+        let contactDetails = {
+            location: location,
+            address: addr,
+            telephone: tel,
+            email: email
+        };
+
+        fullBar.contact_details = contactDetails;
 
         //SOCIAL
 
+        let social = [] as Social[]
+
         let fbHtml = $(barDeetsHtml).find("a[href*=facebook\\.com]");
         if(fbHtml.length == 1 ){
-            fullBar.social.push( {
+            social.push( {
                 network: "facebook",
                 handle: fbHtml.text(),
                 url: fbHtml.attr("href")
@@ -109,7 +130,7 @@ let requestBarDetails = (barBase: BarBase): Bar => {
 
         let twitterHtml = $(barDeetsHtml).find("a[href*=twitter\\.com]");
         if(twitterHtml.length == 1) {
-            fullBar.social.push( {
+            social.push( {
                 network: "twitter",
                 handle: twitterHtml.text(),
                 url: twitterHtml.attr("href")
@@ -118,30 +139,35 @@ let requestBarDetails = (barBase: BarBase): Bar => {
 
         let instaHtml = $(barDeetsHtml).find("a[href*=instagram\\.com]");
         if(instaHtml.length == 1) {
-            fullBar.social.push( {
+            social.push( {
                 network: "instagram",
                 handle: instaHtml.text(),
                 url: twitterHtml.attr("href")
             });
         }
+
+        fullBar.social = social;
         
         //Opening hours
+
+        let openingHours = [] as OpeningHours[];
+
         let openingHoursTable = $(barDeetsHtml).find("table.opening-hours");
         let openingHoursTableRows = $(openingHoursTable).find("tr");
+        
         openingHoursTableRows.each( (index, row) => {
             let day = $(row).find("th").text();
-            let times = $(row).find("td").text().split("-");
+            let times = $(row).find("td").text();
 
-            fullBar.opening_hours.push({
+            openingHours.push({
                 day: day,
-                timeOpen: times[0].trim(),
-                timeClose: times[1].trim()
+                times: times
             });
         });
+        fullBar.opening_hours = openingHours;
 
-
+        return fullBar;
    });
-    return fullBar;
 }
 
 //Cloudflare email hack. Taken from https://usamaejaz.com/cloudflare-email-decoding/
@@ -154,4 +180,28 @@ function cfDecodeEmail(encodedString: string): string {
     return email;
 }
 
+console.log("jebote led");
+console.log(process.cwd());
 
+
+const fullBars = Array<Bar>();
+
+// let baseBarsBuffer = fs.readFileSync("../static/bars.json");
+fs.readFile("../static/bars.json", (err, data) => {
+
+    if(err) {
+        console.log(err);
+        return;
+    }
+
+    let baseBars = JSON.parse(data.toString()) as Array<BarBase>;
+    // baseBars.forEach( bar => fullBars.push(requestBarDetails(bar)));
+    requestBarDetails(baseBars[0]).then( bar => {
+        fullBars.push(bar);
+    })
+    
+
+
+    fs.writeFileSync("../static/bars-full.json", JSON.stringify(fullBars));
+
+})
