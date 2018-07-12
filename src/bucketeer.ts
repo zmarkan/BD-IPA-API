f2658dbe5473bd0b6fc88074ed436140c94613cf
Zan Markan <zan.markan@gmail.com>
2018-07-12 09:03:03 +0100

    wip storage thingy

--- src/bucketeer.ts
+++ src/bucketeer.ts
@@ -2,6 +2,55 @@ import * as aws from 'aws-sdk';
 import { Bar } from './bar';
 import { resolve } from 'url';
 
+//TODO
+
+//Files
+const LIMITED_LIST = "";
+const FULL_LIST = "";
+
+export interface Storage {
+
+    storeFullBarDetails: (bars: Array<Bar>) => Promise<Boolean>;
+    storeLimitedBarDetails: (bars: Array<Bar>) => Promise<Boolean>;
+
+    retrieveFullBarDetails: () => Promise<Array<Bar>>;
+    retrieveLimitedBarDetails: () => Promise<Array<Bar>>;
+}
+
+class BucketStorage implements Storage  {
+    
+    storeFullBarDetails: (bars: Bar[]) => Promise<Boolean> = (bars) => {
+
+        return new Promise<Boolean> ( (resolve, reject) => {
+        });
+    }
+    
+    storeLimitedBarDetails: (bars: Bar[]) => Promise<Boolean> = (bars) => {
+
+        return new Promise<Boolean> ( (resolve, reject) => {
+
+        });
+    }
+
+    retrieveFullBarDetails: () => Promise<Bar[]> = () => {
+        return this.retrieveBarsListFromBucket(FULL_LIST);
+    };
+
+    retrieveLimitedBarDetails: () => Promise<Bar[]> = () => {
+        return this.retrieveBarsListFromBucket(LIMITED_LIST);
+    };
+
+    private retrieveBarsListFromBucket = (filename: string) => {
+        return new Promise<Bar[]> ( (resolve, reject) => {});
+    }
+
+
+}
+
+
+
+
+
 export const storeFullBarDetails = (bars: Array<Bar>) => {
 
 
